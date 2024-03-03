bcb119c783a24f7bbae71c6310002d9ee588baae
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2024-03-03 19:57:41 +0100

    Fix mpp publishing (#6)
    Co-authored-by: hfhbd <hfhbd@users.noreply.github.com>

--- gradle/build-logic/src/main/kotlin/jvm.gradle.kts
+++ gradle/build-logic/src/main/kotlin/jvm.gradle.kts
@@ -1,25 +1,18 @@
-import org.jetbrains.dokka.gradle.*
-
 plugins {
     kotlin("jvm")
     id("setup")
     id("app.softwork.serviceloader")
     id("com.google.devtools.ksp")
-    id("org.jetbrains.dokka")
 }
 
 kotlin {
     setup()
 }
 
-tasks.named<DokkaTaskPartial>("dokkaHtmlPartial") {
-    dokkaSourceSets.configureEach {
-        externalDocumentationLink("https://cashapp.github.io/sqldelight/2.0.0/2.x/")
-        externalDocumentationLink(
-            url = "https://kotlinlang.org/api/kotlinx-datetime/",
-            packageListUrl = "https://kotlinlang.org/api/kotlinx-datetime/kotlinx-datetime/package-list",
-        )
-        externalDocumentationLink("https://uuid.softwork.app")
-        externalDocumentationLink("https://kotlinlang.org/api/kotlinx.coroutines/")
+publishing {
+    if (name != "gradle-plugin") {
+        publications.register<MavenPublication>("mavenJava") {
+            from(components["java"])
+        }
     }
 }
--- gradle/build-logic/src/main/kotlin/setup.gradle.kts
+++ gradle/build-logic/src/main/kotlin/setup.gradle.kts
@@ -1,11 +1,18 @@
+import org.jetbrains.dokka.gradle.*
+
 plugins {
     id("publish")
+    id("org.jetbrains.dokka")
 }
 
-publishing {
-    if (name != "gradle-plugin") {
-        publications.register<MavenPublication>("mavenJava") {
-            from(components["java"])
-        }
+tasks.named<DokkaTaskPartial>("dokkaHtmlPartial") {
+    dokkaSourceSets.configureEach {
+        externalDocumentationLink("https://cashapp.github.io/sqldelight/2.0.0/2.x/")
+        externalDocumentationLink(
+            url = "https://kotlinlang.org/api/kotlinx-datetime/",
+            packageListUrl = "https://kotlinlang.org/api/kotlinx-datetime/kotlinx-datetime/package-list",
+        )
+        externalDocumentationLink("https://uuid.softwork.app")
+        externalDocumentationLink("https://kotlinlang.org/api/kotlinx.coroutines/")
     }
 }
