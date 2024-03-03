254d9c30c885ff7099d5414d9562642a076dfcf0
hfhbd <hfhbd@users.noreply.github.com>
2024-03-03 19:09:38 +0100

    Fix mavencentral publishing

--- gradle/build-logic/build.gradle.kts
+++ gradle/build-logic/build.gradle.kts
@@ -6,6 +6,8 @@ dependencies {
     implementation(libs.plugins.kotlin.jvm.toDep())
     implementation(libs.plugins.serviceloader.toDep())
     implementation(libs.plugins.ksp.toDep())
+    implementation(libs.plugins.publish.toDep())
+    implementation(libs.plugins.dokka.toDep())
 }
 
 fun Provider<PluginDependency>.toDep() = map {
