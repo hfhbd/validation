948e447d44bd585c881d1691836029198c5f8183
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2024-04-24 07:57:16 +0000

    Add @MustBeDocumented to be included in Dokka

--- runtime/src/commonMain/kotlin/app/softwork/validation/annotations.kt
+++ runtime/src/commonMain/kotlin/app/softwork/validation/annotations.kt
@@ -1,7 +1,9 @@
 package app.softwork.validation
 
+@MustBeDocumented
 @Target(AnnotationTarget.PROPERTY)
 public annotation class MinLength(val inclusive: Int)
 
+@MustBeDocumented
 @Target(AnnotationTarget.PROPERTY)
 public annotation class MaxLength(val inclusive: Int)
