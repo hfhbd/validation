df5eba90897fc238a2619abf6b7723a38983b898
hfhbd <22521688+hfhbd@users.noreply.github.com>
2026-04-07 16:12:07 +0200

    Bump Kotlin to 2.4.0-Beta1

--- gradle/build-logic/src/main/kotlin/kotlin.kt
+++ gradle/build-logic/src/main/kotlin/kotlin.kt
@@ -1,6 +1,12 @@
 import org.jetbrains.kotlin.gradle.dsl.KotlinProjectExtension
+import org.jetbrains.kotlin.gradle.dsl.abi.ExperimentalAbiValidation
 
 fun KotlinProjectExtension.setup() {
     jvmToolchain(8)
     explicitApi()
+
+    @OptIn(ExperimentalAbiValidation::class)
+    abiValidation {
+        enabled.set(true)
+    }
 }
